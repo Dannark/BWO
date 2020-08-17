8a7a9ae861d0c275ec2f8cd4f70c1b69ee4d05e9
Daniel Queiroz <daniel.queiroz789@gmail.com>
2020-08-17 19:16:21 -0300

    Initial Server configuration for heroku

--- bwo-server/server.js
+++ bwo-server/server.js
@@ -2,25 +2,12 @@
 import fs from 'fs'
 import startServer from './core/core_server.js'
 
-//1. Load the initializers
-var init_files = fs.readdirSync('./initializers/')
-init_files.forEach(initFile => {
-    console.log('Loading Initializer: '+initFile);
-    //require('./initializers/'+initFile)
-});
-
-//2. load data models
-var model_files = fs.readdirSync('./models/')
-model_files.forEach(modelFiles => {
-    console.log('Loading Models: '+modelFiles)
-});
- 
 //3. load game maps data
-const maps = {name:"main"}
-var map_files = fs.readdirSync('./resources/data/maps/')
-map_files.forEach(mapFiles => {
-    console.log('Loading map: '+mapFiles)
-});
+// const maps = {name:"main"}
+// var map_files = fs.readdirSync('./resources/data/maps/')
+// map_files.forEach(mapFiles => {
+//     console.log('Loading map: '+mapFiles)
+// });
 
 //4. Initiate the server and listen to the internets
     //all of server logic
